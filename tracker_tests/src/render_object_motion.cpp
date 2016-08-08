cad5e03c75856d1126e07466c7ac78cb21163bab
Alessandro Pieropan <CoffeRobot@users.noreply.github.com>
2016-08-09 08:40:31 +0900

    rendering fix

---
+++ tracker_tests/src/render_object_motion.cpp
@@ -0,0 +1,309 @@
+/*****************************************************************************/
+/*  Copyright (c) 2016, Alessandro Pieropan                                  */
+/*  All rights reserved.                                                     */
+/*                                                                           */
+/*  Redistribution and use in source and binary forms, with or without       */
+/*  modification, are permitted provided that the following conditions       */
+/*  are met:                                                                 */
+/*                                                                           */
+/*  1. Redistributions of source code must retain the above copyright        */
+/*  notice, this list of conditions and the following disclaimer.            */
+/*                                                                           */
+/*  2. Redistributions in binary form must reproduce the above copyright     */
+/*  notice, this list of conditions and the following disclaimer in the      */
+/*  documentation and/or other materials provided with the distribution.     */
+/*                                                                           */
+/*  3. Neither the name of the copyright holder nor the names of its         */
+/*  contributors may be used to endorse or promote products derived from     */
+/*  this software without specific prior written permission.                 */
+/*                                                                           */
+/*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      */
+/*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        */
+/*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    */
+/*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     */
+/*  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   */
+/*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         */
+/*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    */
+/*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    */
+/*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      */
+/*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    */
+/*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     */
+/*****************************************************************************/
+
+#include "../../fato_rendering/include/multiple_rigid_models_ogre.h"
+#include "../../fato_rendering/include/windowless_gl_context.h"
+#include "../../fato_rendering/include/device_2d.h"
+#include "../../fato_rendering/include/device_1d.h"
+#include "../../tracker/include/flow_graph.hpp"
+#include "../../tracker/include/nvx_utilities.hpp"
+#include "../../cuda/include/utility_kernels.h"
+#include <NVX/nvxcu.h>
+#include <NVX/nvx_opencv_interop.hpp>
+#include <NVX/nvx_timer.hpp>
+#include <string>
+#include <memory>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <chrono>
+
+using namespace std;
+using namespace cv;
+
+
+// default parameters
+int width = 640;
+int height = 480;
+double fx = 500.0;
+double fy = 500.0;
+double cx = width / 2.0;
+double cy = height / 2.0;
+double near_plane = 0.01;  // for init only
+double far_plane = 10.0;   // for init only
+Eigen::Matrix<double, 3, 8> bounding_box;
+
+
+void getZshif(float rot_x, float rot_y, float scale_apperance,
+              Eigen::Translation<double, 3> &tra_center,
+              Eigen::Transform<double, 3, Eigen::Affine> &t_render,
+              double z_shift)
+{
+
+//    // rotate around x to 'render' just as Ogre
+//    Eigen::Matrix3d Rx_180 = Eigen::Matrix<double, 3, 3>::Identity();
+//    Rx_180(1, 1) = -1.0;
+//    Rx_180(2, 2) = -1.0;
+//    rot_view *= Rx_180;
+
+//    // apply tra_center -> rot_view to bounding box
+//    Eigen::Transform<double, 3, Eigen::Affine> t = rot_view * tra_center;
+//    Eigen::Matrix<double, 3, 8> bb;
+//    bb = t * bounding_box;
+
+//    // compute minimal z-shift required to ensure visibility
+//    // assuming cx,cy in image center
+//    Eigen::Matrix<double, 1, 8> shift_x =
+//        (2.0 * fx / (double)width) * bb.row(0).array().abs();
+//    shift_x -= bb.row(2);
+//    Eigen::Matrix<double, 1, 8> shift_y =
+//        (2.0 * fy / (double)height) * bb.row(1).array().abs();
+//    shift_y -= bb.row(2);
+//    Eigen::Matrix<double, 1, 16> shift;
+//    shift << shift_x, shift_y;
+//    double z_shift = shift.maxCoeff();
+}
+
+
+void getPose(float rot_x, float rot_y,
+             Eigen::Translation<double, 3> &tra_center,
+             Eigen::Transform<double, 3, Eigen::Affine> &t_render) {
+  Eigen::Matrix3d rot_view;
+  rot_view = Eigen::AngleAxisd(rot_y, Eigen::Vector3d::UnitY()) *
+             Eigen::AngleAxisd(rot_x, Eigen::Vector3d::UnitX());
+
+  // rotate around x to 'render' just as Ogre
+  Eigen::Matrix3d Rx_180 = Eigen::Matrix<double, 3, 3>::Identity();
+  Rx_180(1, 1) = -1.0;
+  Rx_180(2, 2) = -1.0;
+  rot_view *= Rx_180;
+
+  // apply tra_center -> rot_view to bounding box
+  Eigen::Transform<double, 3, Eigen::Affine> t = rot_view * tra_center;
+  Eigen::Matrix<double, 3, 8> bb;
+  bb = t * bounding_box;
+
+  // compute minimal z-shift required to ensure visibility
+  // assuming cx,cy in image center
+  Eigen::Matrix<double, 1, 8> shift_x =
+      (2.0 * fx / (double)width) * bb.row(0).array().abs();
+  shift_x -= bb.row(2);
+  Eigen::Matrix<double, 1, 8> shift_y =
+      (2.0 * fy / (double)height) * bb.row(1).array().abs();
+  shift_y -= bb.row(2);
+  Eigen::Matrix<double, 1, 16> shift;
+  shift << shift_x, shift_y;
+  double z_shift = shift.maxCoeff();
+  Eigen::Translation<double, 3> tra_z_shift(0, 0, z_shift);
+
+  // compute bounding box limits after z-shift
+  near_plane = (bb.row(2).array() + z_shift).minCoeff();
+  far_plane = (bb.row(2).array() + z_shift).maxCoeff();
+
+  cout << "near " << near_plane << endl;
+  cout << "far" << far_plane << "\n\n";
+
+  // compose render transform (tra_center -> rot_view -> tra_z_shift)
+  t_render = tra_z_shift * rot_view * tra_center;
+}
+
+
+void renderObject(Eigen::Transform<double, 3, Eigen::Affine> &pose,
+                  pose::MultipleRigidModelsOgre &model_ogre) {
+  double tra_render[3];
+  double rot_render[9];
+  Eigen::Map<Eigen::Vector3d> tra_render_eig(tra_render);
+  Eigen::Map<Eigen::Matrix<double, 3, 3, Eigen::RowMajor>> rot_render_eig(
+      rot_render);
+  tra_render_eig = pose.translation();
+  rot_render_eig = pose.rotation();
+
+  std::vector<pose::TranslationRotation3D> TR(1);
+  TR.at(0).setT(tra_render);
+  TR.at(0).setR_mat(rot_render);
+
+  model_ogre.updateProjectionMatrix(fx, fy, cx, cy, near_plane, far_plane);
+  model_ogre.render(TR);
+}
+
+void downloadRenderedImg(pose::MultipleRigidModelsOgre &model_ogre,
+                         std::vector<uchar4> &h_texture) {
+  util::Device1D<uchar4> d_texture(height * width);
+  vision::convertFloatArrayToGrayRGBA(d_texture.data(), model_ogre.getTexture(),
+                                      width, height, 1.0, 2.0);
+  h_texture.resize(height * width);
+  d_texture.copyTo(h_texture);
+}
+
+
+void testObjectMovement() {
+  render::WindowLessGLContext dummy(10, 10);
+
+//  int image_w = 640;
+//  int image_h = 480;
+//  double focal_length_x = 632.7361080533549;
+//  double focal_length_y = 634.2327075892116;
+//  double nodal_point_x = 321.9474832561696;
+//  double nodal_point_y = 223.9353111003978;
+
+  string filename =
+      "/home/alessandro/projects/drone_ws/src/fato_tracker/data/kellog/"
+      "kellog.obj";
+
+  pose::MultipleRigidModelsOgre rendering_engine(width, height, fx, fy, cx, cy,
+                                           near_plane, far_plane);
+
+  rendering_engine.addModel(filename);
+
+  // object vertices
+  const render::RigidObject &obj_model = rendering_engine.getRigidObject(0);
+  Eigen::Map<const Eigen::MatrixXf> vertices_f(obj_model.getPositions().data(),
+                                               3, obj_model.getNPositions());
+  Eigen::MatrixXd vertices;
+  vertices = vertices_f.cast<double>();
+
+  // bounding box
+  Eigen::Map<const Eigen::MatrixXf> bounding_box_f(
+      obj_model.getBoundingBox().data(), 3, 8);
+
+  bounding_box = bounding_box_f.cast<double>();
+
+  cout << " bounding box " << endl;
+  cout << fixed << setprecision(3) << bounding_box << endl;
+
+
+  // centralizing translation
+  auto mn = vertices.rowwise().minCoeff();
+  auto mx = vertices.rowwise().maxCoeff();
+  Eigen::Translation<double, 3> tra_center(-(mn + mx) / 2.0f);
+
+  cout << " translation " << endl;
+  cout << mn << " " << mx << endl;
+  cout << fixed << setprecision(3) << tra_center.x()  << " "
+       << tra_center.y() << " " << tra_center.z() << endl;
+
+  Eigen::Transform<double, 3, Eigen::Affine> t_render;
+  getPose(0, 0, tra_center, t_render);
+  // tra_z_shift * rot_view * tra_center;
+
+  cout << fixed << setprecision(3) << t_render.translation() << "\n" << t_render.rotation() << endl;
+
+  //t_render.translate()
+
+  renderObject(t_render, rendering_engine);
+
+  std::vector<uchar4> h_texture(height * width);
+  downloadRenderedImg(rendering_engine, h_texture);
+
+  cv::Mat img_rgba(height, width, CV_8UC4, h_texture.data());
+  cv::Mat img_gray;
+  cv::cvtColor(img_rgba, img_gray, CV_RGBA2BGR);
+
+
+//  for(auto i = 0; i <8; ++i)
+//  {
+//      cout << fixed << setprecision(3) << tra_center.x() << " " << tra_center.y() << " " << tra_center.z()  << endl;
+//  }
+
+//  vector<double> box = bboxes[0];
+//  cout << bboxes.size() << " " << box.size() << endl;
+
+//  Point2d pt0(box[0],box[1]);
+//  Point2d pt1(box[2],box[3]);
+//  Point2d pt2(box[4],box[5]);
+//  Point2d pt3(box[6],box[7]);
+//  Point2d pt4(box[8],box[9]);
+//  Point2d pt5(box[10],box[11]);
+//  Point2d pt6(box[12],box[13]);
+//  Point2d pt7(box[014],box[15]);
+
+
+//  for(int i = 0; i < 4; i++)
+//  {
+//      circle(rendered_image, Point2f(box[2*i],box[2*i+1]), 3, Scalar(255,0,0), 1);
+//  }
+
+//  for(int i = 0; i < 4; i++)
+//  {
+//      circle(rendered_image, Point2f(box[2*i+4],box[2*i+1+4]), 3, Scalar(255,0,255), 1);
+//  }
+
+//  for(int i = 0; i < 8; ++i)
+//  {
+//      cout << box[2*i] << " " << box[2*i+1] << endl;
+//  }
+
+//  line(rendered_image, pt0, pt2, Scalar(0,255,0), 1);
+//  line(rendered_image, pt2, pt6, Scalar(0,255,0), 1);
+//  line(rendered_image, pt6, pt4, Scalar(0,255,0), 1);
+//  line(rendered_image, pt4, pt0, Scalar(0,255,0), 1);
+
+  imshow("debug", img_gray);
+  waitKey(0);
+
+}
+
+int main(int argc, char** argv) {
+  vx_context context = vxCreateContext();
+
+  // synthgraph_test(context);
+  // realgraph_test(context);
+  // gl2Vx(context);
+  //synthgraph_test2(context);
+
+  testObjectMovement();
+
+  //    cout << VX_FAILURE << endl;
+  //    cout << VX_ERROR_INVALID_REFERENCE << endl;
+  //    cout << VX_ERROR_INVALID_PARAMETERS << endl;
+  //    return 0;
+
+  //    cout << sizeof(nvx_keypointf_t) << " " << sizeof(KeyPoint) << endl;
+
+  //    transferData(context);
+
+  //    return 0;
+
+  // uploadPoints(context);
+
+  //    nvxcu_pitch_linear_image_t image;
+  //    image.base.image_type = NVXCU_PITCH_LINEAR_IMAGE;
+  //    image.base.format = NVXCU_DF_IMAGE_U8;
+  //    image.base.width = image_w;
+  //    image.base.height = image_h;
+  //    image.planes[0].dev_ptr = dev_ptr;
+  //    image.planes[0].pitch_in_bytes = pitch;
+
+  return 0;
+}
